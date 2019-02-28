68cd0773b91c9a3cd893509a08f887ee699af746
Thomas Ranvier <ranvier.thomas.pro@gmail.com>
2019-02-28 22:51:49 +0100

    Changes

--- keymap.c
+++ keymap.c
@@ -49,7 +49,7 @@ bool is_linux = false;// Used to detect on what OS I type and so what kind of sh
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox(
     // left hand
-    KC_NO,       TO(1),       TO(2),       KC_NO,         KC_NO,       KC_NO,       KC_NO,       
+    KC_NO,       CS_ECIRC,      CS_EACUTE,       CS_EGRAVE,         CS_CCEDI,       CS_AGRAVE,       CS_UGRAVE,       
     KC_ESC,       KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,          KC_MINS,       
     KC_QUOT,         KC_A,          KC_S,          KC_D,          KC_F,          KC_G,          
     MO(2),       KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,          KC_TAB,       
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                               KC_NO,       
                                                                 KC_LSFT, MO(1),      KC_LCMD,       
         // right hand
-        KC_NO,       KC_NO,       KC_LEFT,       KC_DOWN,       KC_UP,         KC_RGHT,       KC_NO,       
+        KC_NO,       KC_HOME,       KC_LEFT,       KC_DOWN,       KC_UP,         KC_RGHT,       KC_END,       
         KC_EQL,       KC_Y,          KC_U,          KC_I,          KC_O,          KC_P,          KC_GRAVE,       
                        KC_H,          KC_J,          KC_K,          KC_L,          KC_M,       KC_ENT,         
         KC_SCLN,       KC_N,          KC_COMM,       KC_DOT,        KC_LBRC,       KC_RBRC,      MO(2),       
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_ergodox(
     // left hand
-    KC_NO,       TO(0),         TO(2),         KC_NO,         KC_NO,         KC_NO,         KC_NO,       
+    KC_NO,       KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,       
     KC_ESC,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_NO,       
     KC_NO,       KC_EXLM,       KC_AT,         KC_HASH,       KC_DLR,        KC_PERC,       
     KC_NO,       KC_1,       KC_2,        KC_3,       KC_4,       KC_5,       KC_TAB,       
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT_ergodox(
     // left hand
-    KC_NO,       TO(1),         TO(0),       KC_NO,       KC_NO,       KC_NO,       KC_NO,       
+    KC_NO,       KC_NO,         KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       
     KC_ESC,       KC_NO,       KC_BTN1,       KC_BTN3,       KC_BTN2,       KC_NO,       KC_NO,       
     KC_NO,       KC_MS_L,       KC_MS_D,       KC_MS_U,       KC_MS_R,       KC_NO,       
     KC_NO,       KC_WH_L,       KC_WH_D,       KC_WH_U,       KC_WH_R,       KC_NO,       KC_TAB,       
